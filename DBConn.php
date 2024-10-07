de7db7f4655a1ef731bd3d537fc21b27d0dcce12
Zoe-Hey <150931346+Zoe-Hey@users.noreply.github.com>
2024-10-07 17:51:09 +0200

    finalize dbconn code

--- DBConn.php
+++ DBConn.php
@@ -1,27 +1,20 @@
 <?php
 //get server host, name of user, password, name of database
 //declare varibales for db connection
-$hostname = "";
-$username = "";
+$hostname = "localhost";
+$username = "root";
 $password = "";
 $database = "ClothingStore";
 
-//create db connection variable
-$dbConn = @new mysqli();
-
-//check connection
-if ($dbConn->connect_error) 
-    $errorMgs[] = "The databse server is not available".
-    "Connect Error is " . $mysqli->connect_errno . " " . $mysqli->connect_error . ".";
-else
-    echo "<p> Connection Established </p>";
-
-/*
+//connect to mysql database using PDO connection
+//try to create a new pdo connection using database credentials
 try {
     $db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+    //error handling to throw exceptions, if error happens it will be caught
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
+    //die function outputs message
     die("Error: " . $e->getMessage());
 }
-    */
+
 ?>
