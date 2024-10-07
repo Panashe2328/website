73445e4b73921c4b9e1386bbd47526ddce07b685
Zoe-Hey <150931346+Zoe-Hey@users.noreply.github.com>
2024-10-07 13:45:00 +0200

    putting in code to create connection and echo out status of connection

--- DBConn.php
+++ DBConn.php
@@ -1,9 +1,27 @@
 <?php
-//get server address, name of user, password, name of database
+//get server host, name of user, password, name of database
+//declare varibales for db connection
 $hostname = "";
 $username = "";
 $password = "";
-$database = "";
+$database = "ClothingStore";
 
+//create db connection variable
+$dbConn = @new mysqli();
 
+//check connection
+if ($dbConn->connect_error) 
+    $errorMgs[] = "The databse server is not available".
+    "Connect Error is " . $mysqli->connect_errno . " " . $mysqli->connect_error . ".";
+else
+    echo "<p> Connection Established </p>";
+
+/*
+try {
+    $db = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Error: " . $e->getMessage());
+}
+    */
 ?>
