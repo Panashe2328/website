f871dd4ab72b90cd8f5abfe8b62be81f68e42d1e
Zoe-Hey <150931346+Zoe-Hey@users.noreply.github.com>
2024-10-09 22:27:39 +0200

    db

--- DBConn.php
+++ DBConn.php
@@ -1,21 +1,21 @@
 <?php
-// Database connection details
+//database connection details
 $hostname = "127.0.0.1"; // Use the IP address if localhost doesn't work
 $username = "root";
 $password = "";
 $database = "clothingstore";
 
 try {
-    // Establish a connection using PDO
+    //establish a connection using PDO
     $db = new PDO("mysql:host=$hostname;dbname=$database;charset=utf8mb4", $username, $password);
     
-    // Set PDO to throw exceptions on error
+    //use PDO to throw exceptions on error
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Display connection success message
+    //show connection success message
     echo "<p>Connection successful!</p>";
 
-    // Check if the table 'tblUser' exists
+    //check if the table 'tblUser' exists
     $query = $db->query("SHOW TABLES LIKE 'tblUser'");
     
     if ($query->rowCount() > 0) {
@@ -24,8 +24,32 @@ try {
         echo "Table 'tblUser' does not exist.";
     }
 
+    //check if the table 'tblAdmin' exists
+    $query = $db->query("SHOW TABLES LIKE 'tblAdmin'");
+    if ($query->rowCount() > 0) {
+        echo "Table 'tblAdmin' exists in the database.<br>";
+    } else {
+        echo "Table 'tblAdmin' does not exist.<br>";
+    }
+
+    //check if the table 'tblClothes' exists
+    $query = $db->query("SHOW TABLES LIKE 'tblClothes'");
+    if ($query->rowCount() > 0) {
+        echo "Table 'tblClothes' exists in the database.<br>";
+    } else {
+        echo "Table 'tblClothes' does not exist.<br>";
+    }
+
+    //check if the table 'tblOrder' exists
+    $query = $db->query("SHOW TABLES LIKE 'tblOrder'");
+    if ($query->rowCount() > 0) {
+        echo "Table 'tblOrder' exists in the database.<br>";
+    } else {
+        echo "Table 'tblOrder' does not exist.<br>";
+    }
+
 } catch (PDOException $e) {
-    // Output the error message if the connection fails
+    //Show the error message if the connection fails
     die("Error: " . $e->getMessage());
 }
 
