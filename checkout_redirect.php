36203574f5f9dd23832a40eb91116de060a39df5
Zoe-Hey <150931346+Zoe-Hey@users.noreply.github.com>
2024-11-13 14:30:49 +0200

    new

--- checkout_redirect.php
+++ checkout_redirect.php
@@ -2,7 +2,7 @@
 session_start();
 
 // Check if user is logged in
-if (!isset($_SESSION['user_id']) || !isset($_SESSION['username'])) {
+if (!isset($_SESSION['user_id'])) {
     // If not logged in, redirect to login page
     header("Location: login.php"); 
     exit;
